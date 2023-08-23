c50fe73fbed139d8652b2d334b79a2f7d089aba6
Therese <hernando.theresechristine.t@gmail.com>
2023-08-23 08:44:35 +0800

    feat[record-card]: implement tooltip for record card title, refactor emotion chip tooltip implementation

---
+++ src/app/records/record-card/record-card-title.constant.ts
@@ -0,0 +1,6 @@
+/**
+ * Character limit to show in RecordCardComponent
+ * exceeding this an ellipsis will be shown
+ * using textOverflow.
+ */
+export const RECORD_CARD_TITLE = 41;
--- src/app/records/record-card/record-card.component.ts
+++ src/app/records/record-card/record-card.component.ts
@@ -3,6 +3,7 @@ import { ThoughtRecord } from 'src/app/core/models';
 import { ThoughtRecordUi } from 'src/app/core/models/thought-record-ui.interface';
 
 import { ThoughtRecordThoughtRecordUiMapperService } from '../services/thought-record-thought-record-ui-mapper.service';
+import { RECORD_CARD_TITLE } from './record-card-title.constant';
 
 
 @Component({
@@ -13,6 +14,7 @@ import { ThoughtRecordThoughtRecordUiMapperService } from '../services/thought-r
 export class RecordCardComponent implements OnInit {
   @Input({ required: true }) thoughtRecord!: ThoughtRecord;
   thoughtRecordUi!: ThoughtRecordUi;
+  readonly RECORD_CARD_TITLE = RECORD_CARD_TITLE;
 
   constructor(private mapperService: ThoughtRecordThoughtRecordUiMapperService) { }
 
