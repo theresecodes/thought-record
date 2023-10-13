0383d9d68fe7650eaa358e6c113358a7eab11801
Therese <hernando.theresechristine.t@gmail.com>
2023-10-13 08:47:12 +0800

    refactor[add-record-structure]: use a tag instead of button on navigation to features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 import { Features } from './core/constants/features.constant';
-import { Feature } from './core/enums/feature.enum';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +10,4 @@ import { Feature } from './core/enums/feature.enum';
 export class AppComponent {
   title = 'Thought Record App';
   readonly features = Features;
-
-  constructor(private router: Router) { }
-
-  navigateTo(feature: string): void {
-    switch (feature) {
-      case Feature.Records:
-        this.router.navigateByUrl("/records");
-        break;
-      case Feature.Graph:
-        this.router.navigateByUrl("/graph");
-        break;
-      case Feature.Export:
-        this.router.navigateByUrl("/export");
-        break;
-    }
-  }
 }
